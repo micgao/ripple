c8fe9250f9daee90aec7d8160da88a2b6f223033
Michael G <micgao@ripplestud.io>
2022-07-15 00:04:32 -0400

    Content work

--- assets/js/formhandler.js
+++ assets/js/formhandler.js
@@ -10,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function() {
   }
 
   function error() {
-    status.innerHTML = "Oops! An error occurred. Please try again, or feel free to send an email.";
+    status.innerHTML = "Oops! An error occurred. Please try again, or feel free to reach out by email.";
   }
 
   // handle the form submission event
